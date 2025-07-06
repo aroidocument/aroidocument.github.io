847a8c47e6e118573668731158b4a815fedd61d2
corkborg <62742115+corkborg@users.noreply.github.com>
2025-07-06 14:00:24 +0900

    Fix.

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import path from "path";
 import * as fs from 'fs/promises';
 const mkdirp = require("mkdirp");
 import { globFiles } from "./src/pageRepository";
-import { createPagemenu as createPageTreeHtml } from "./src/pagemenuHtmlService";
+import { pageTreeFactory } from "./src/pagemenuHtmlService";
 
 const asciidoctor = _asciidoctor();
 
@@ -14,53 +14,70 @@ const articleDir = "article";
 const templateDir = "template";
 
 const main = async () => {
-  // 出力先のリセット
+  // 出力先の初期化
   await fs.rm(distDir, { recursive: true, force: true });
   await mkdirp(distDir);
 
-  const sidemenuDom = await createPageTreeHtml(articleDir);
+  const pageTree = await pageTreeFactory(articleDir);
+  const pageTreeHtml = await pageTree.createPageTreeHtml();
 
   // ページ間で共通的に使えるマクロ要素
   let commonAttribute = {
-    "sidemenu": sidemenuDom.window.document.body.innerHTML
+    "sidemenu": pageTreeHtml
   };
 
-  // ページの出力を行う
+  // asciidoc事にページの出力を行う
   const articlePaths = await globFiles(path.join(articleDir, "**/*.adoc"));
   for(const asciidocPath of articlePaths) {
-    const doc = asciidoctor.loadFile(asciidocPath);
-
-    // 記事にマクロの適応を行う
-    let htmlArticle = nunjucks.renderString(doc.convert(), {
-      ...commonAttribute,
-      //pagemenu: pageMenuDom.window.document.body.innerHTML
-    });
-
-    // ページ毎に定義されたマクロ要素
-    const pageAttribute = {
-      article: htmlArticle,
-      title: doc.getDocumentTitle(),
-      ...commonAttribute,
-    }
-
-    // ページ全体にマクロを適応する
-    let resultHtml = nunjucks.render(`${templateDir}/index.html`,  pageAttribute);
-
-    // ファイルの出力先ディレクトリを確定する。入れ子構造になっている場合には入れ子先のディレクトリを指す
-    const outputDir = path.join(distDir, path.relative(articleDir, path.dirname(asciidocPath)));
-    const outputFilename = path.parse(asciidocPath).name + '.html';  // 元ファイル名にhtml拡張子をつける
-    const outputPath = path.join(outputDir, outputFilename);
-
-    // 結果の出力
-    await outputHtml(outputPath, resultHtml);
+    await generatePageHtml(asciidocPath, commonAttribute);
   }
 
-  // faviconの設定
+  const sitemapXml = pageTree.createSitemapXml();
+  outputHtml(path.join(distDir, 'sitemap.xml'), sitemapXml);
+
+  // 静的ファイルのコピー
+  await staticFileCopy();
+  console.log('Done.');
+}
+
+async function generatePageHtml(asciidocPath: string, commonAttribute: any) {
+  const doc = asciidoctor.loadFile(asciidocPath);
+
+  // 記事にマクロの適応を行う
+  let htmlArticle = nunjucks.renderString(doc.convert(), {
+    ...commonAttribute,
+    //pagemenu: pageMenuDom.window.document.body.innerHTML
+  });
+
+  // ページ毎に定義されたマクロ要素
+  const pageAttribute = {
+    article: htmlArticle,
+    title: doc.getDocumentTitle(),
+    ...commonAttribute,
+  }
+
+  // ページ全体にマクロを適応する
+  let resultHtml = nunjucks.render(`${templateDir}/index.html`,  pageAttribute);
+
+  // ファイルの出力先ディレクトリを確定する。入れ子構造になっている場合には入れ子先のディレクトリを指す
+  const outputDir = path.join(distDir, path.relative(articleDir, path.dirname(asciidocPath)));
+  const outputFilename = path.parse(asciidocPath).name + '.html';  // 元ファイル名にhtml拡張子をつける
+  const outputPath = path.join(outputDir, outputFilename);
+
+  // 結果の出力
+  await outputHtml(outputPath, resultHtml);
+}
+
+/**
+ * 静的ファイルのコピーを行う
+ */
+async function staticFileCopy() {
   try {
     await fs.copyFile(path.join(templateDir, 'favicon.ico'), path.join(distDir, 'favicon.ico'));
     await fs.copyFile(path.join(templateDir, 'style.css'), path.join(distDir, 'style.css'));
     await fs.copyFile(path.join(templateDir, 'logo.png'), path.join(distDir, 'logo.png'));
-    console.log('Done.');
+    await fs.copyFile(path.join(templateDir, 'sitemap.xsl'), path.join(distDir, 'sitemap.xsl'));
+    await copyDir(path.join('article', 'img'), path.join(distDir, 'img'));
   }catch (err) {
     console.log(err);
     if(err instanceof Error) {
@@ -69,11 +86,8 @@ const main = async () => {
   }
 }
 
-main()
-
 /**
  * HTMLの出力を行う
- * TODO: outputPathからoutputDirを計算するようにした方が良い
  */
 async function outputHtml(outputPath: string, htmlString: string) {
   // 書き出し先ディレクトリの作成
@@ -88,3 +102,32 @@ async function outputHtml(outputPath: string, htmlString: string) {
     console.log('error:', outputPath); throw err;
   }
 }
+
+export const copyDir = async (src: string, dest: string) => {
+  try {
+    // コピー先ディレクトリが存在しない場合、作成する
+    await fs.mkdir(dest, { recursive: true });
+
+    // コピー元のファイル・ディレクトリ一覧を取得する
+    const files = await fs.readdir(src, { withFileTypes: true });
+
+    // 各エントリに対してコピー操作を実行する
+    for (const file of files) {
+      const srcPath = path.join(src, file.name);
+      const destPath = path.join(dest, file.name);
+
+      if (file.isDirectory()) {
+        // ディレクトリの場合、再帰的にコピーする
+        await copyDir(srcPath, destPath);
+      } else {
+        // ファイルの場合、単純にコピーする
+        await fs.copyFile(srcPath, destPath);
+      }
+    }
+  } catch (error) {
+    console.error(`Error while copying directory: ${error}`);
+  }
+};
+
+
+main()
